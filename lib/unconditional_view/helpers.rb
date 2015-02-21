6566dd6070abf48fee5cab5a688a74a6a1fb9e1f
James Martin <james@roomz.com>
2015-02-21 21:28:46 +1100

    Sketch of partial depending on local and ivar dependencies and deps with a dependable? interface

--- lib/unconditional_view/helpers.rb
+++ lib/unconditional_view/helpers.rb
@@ -1,12 +1,39 @@
 module UnconditionalView
   module ActionView
     module Helpers
+      def depend_on(things)
+        self.class.instance_variable_set(:@_locals, things)
+      end
+
+      def local_dependencies
+        self.class.instance_variable_get(:@_locals)
+      end
+
       def with_local(symbol)
-        yield assigns[symbol]
+        dependency = local_dependencies[symbol]
+        if dependency.respond_to?(:dependable?)
+          if dependency.dependable?
+            yield dependency.renderable_value
+          end
+        elsif dependency.present?
+          yield dependency
+        end
       end
 
       def without_local(symbol)
-        yield
+        dependency = local_dependencies[symbol]
+        if dependency.blank? || (dependency.respond_to?(:dependable?) && !dependency.dependable?)
+          yield
+        end
+      end
+
+      def with_global(symbol)
+        dependency = ActiveSupport::HashWithIndifferentAccess.new(assigns)[symbol]
+        yield dependency unless dependency.blank?
+      end
+
+      def without_global(symbol)
+        yield unless ActiveSupport::HashWithIndifferentAccess.new(assigns)[symbol]
       end
     end
   end
