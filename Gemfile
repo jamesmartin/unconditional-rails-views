8ea2fbf65311e04e7efdfd78661b39bb401cfb68
James Martin <james@roomz.com>
2015-02-21 11:13:58 +1100

    Skeleton Rails app

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+ruby '2.1.5'
+
+gem 'rails', '4.0.3'
+gem 'dotenv-rails', :groups => [:development, :test] # must load early to satisfy ENV dependencies
+
+gem 'sqlite3'
+gem 'uglifier'
+
+group :test, :development do
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsSandbox::Application.config.secret_key_base = '0a5fd4d394b8947ccd8256ad6123bab54395369105d8e2ef25d1e6ac7eab9192ce99d642b8927fc22b050b8f0432964ddf2c43361849cf99efe46db143366b8b'
